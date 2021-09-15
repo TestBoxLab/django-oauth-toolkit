4bec692e183b38501785f802ec1f59dfff73d246
Jake Wood <jake@jakewood.org>
2021-09-15 00:36:03 -0400

    Optional slashes

--- oauth2_provider/urls.py
+++ oauth2_provider/urls.py
@@ -7,10 +7,10 @@ app_name = "oauth2_provider"
 
 
 base_urlpatterns = [
-    re_path(r"^authorize/$", views.AuthorizationView.as_view(), name="authorize"),
-    re_path(r"^token/$", views.TokenView.as_view(), name="token"),
-    re_path(r"^revoke_token/$", views.RevokeTokenView.as_view(), name="revoke-token"),
-    re_path(r"^introspect/$", views.IntrospectTokenView.as_view(), name="introspect"),
+    re_path(r"^authorize/?$", views.AuthorizationView.as_view(), name="authorize"),
+    re_path(r"^token/?$", views.TokenView.as_view(), name="token"),
+    re_path(r"^revoke_token/?$", views.RevokeTokenView.as_view(), name="revoke-token"),
+    re_path(r"^introspect/?$", views.IntrospectTokenView.as_view(), name="introspect"),
 ]
 
 
@@ -32,12 +32,12 @@ management_urlpatterns = [
 
 oidc_urlpatterns = [
     re_path(
-        r"^\.well-known/openid-configuration/$",
+        r"^\.well-known/openid-configuration/?$",
         views.ConnectDiscoveryInfoView.as_view(),
         name="oidc-connect-discovery-info",
     ),
     re_path(r"^\.well-known/jwks.json$", views.JwksInfoView.as_view(), name="jwks-info"),
-    re_path(r"^userinfo/$", views.UserInfoView.as_view(), name="user-info"),
+    re_path(r"^userinfo/?$", views.UserInfoView.as_view(), name="user-info"),
 ]
 
 
